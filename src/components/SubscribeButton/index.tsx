73bef67c4ebc5ca8df052b5085f1788611b68ee4
Gerson Rocha <gersonrocha9@gmail.com>
2022-08-21 13:28:18 -0300

    creating subscripe logic on Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,34 @@
+import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
 import styles from "./styles.module.scss";
+import { getStripeJs } from "../../services/stripe-js";
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("google");
+      return;
+    }
+
+    const response = await api.post("/subscribe", { user: session.user });
+
+    const { sessionId } = response.data;
+
+    const stripe = await getStripeJs();
+
+    await stripe.redirectToCheckout({
+      sessionId: sessionId,
+    });
+  }
+
   return (
-    <button type="button" className={styles.subscribeNowButton}>
+    <button type="button" className={styles.subscribeNowButton} onClick={handleSubscribe}>
       Subscribe now
     </button>
   );
